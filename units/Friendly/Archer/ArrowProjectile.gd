fd12269f1e1a74afa23ecdcf291cc8e2f61477d2
Anna Gluszak <annaglusz@gmail.com>
2022-11-30 17:38:47 -0600

    small archer updates

--- units/Friendly/Archer/ArrowProjectile.gd
+++ units/Friendly/Archer/ArrowProjectile.gd
@@ -5,11 +5,6 @@ const THROW_VELOCITY = Vector2(800,-400)
 
 func throw_arrow(direction, speed):
 	velocity = direction
-	
-
-func _on_Area2D_body_entered(body):
-	pass # Replace with function body.
-
 
 func _on_Area2D_area_entered(area):
 	pass # Replace with function body.
